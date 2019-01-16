500013391d3acde8364a3bc8171da1358d365be5
David <d.sanchez38@hotmail.fr>
2019-01-16 09:57:44 +0100

    Update MainActivity.java

--- MyApplication/app/src/main/java/com/example/kandels/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/kandels/myapplication/MainActivity.java
@@ -39,8 +39,6 @@ import android.widget.SimpleExpandableListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
